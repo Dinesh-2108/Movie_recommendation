437c854f924a5324c55c1263154a0007d7334d43
Dinesh Regidi <152353773+Dinesh-2108@users.noreply.github.com>
2024-08-02 23:19:24 +0530

    pages update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-about = st.Page("about.py",title="About",icon="🏠")
-user = st.Page("account.py", title="Account", icon=":material/add_circle:",default=True)
-help = st.Page("help.py", title="Help", icon="⚙️")
-glossary = st.Page("glossary.py",title ="Glossary",icon="📂")
+about = st.Page("pages/about.py",title="About",icon="🏠")
+user = st.Page("pages/account.py", title="Account", icon=":material/add_circle:",default=True)
+help = st.Page("pages/help.py", title="Help", icon="⚙️")
+glossary = st.Page("pages/glossary.py",title ="Glossary",icon="📂")
 pg = st.navigation([about,user, help,glossary])
 # st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
 st.set_page_config(page_title="Recommendation engine",page_icon=":movie_camera")
